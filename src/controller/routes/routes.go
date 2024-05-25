b47f3f89680f41c9bdfcb857d2804d750a1f2aaf
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-25 14:15:54 -0300

    fix: ajuste da rota para buscar email

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -8,7 +8,7 @@ import (
 func InitRoutes(r *gin.RouterGroup) {
 
 	r.GET("/user/:userId", userController.FindUserById)
-	r.GET("/user/:userEmail", userController.FindUserByEmail)
+	r.GET("/user/email/:userEmail", userController.FindUserByEmail)
 	r.POST("/user", userController.CreateUser)
 	r.PUT("/user/:userId", userController.UpdateUser)
 	r.DELETE("/user/:userId", userController.DeleteUser)
