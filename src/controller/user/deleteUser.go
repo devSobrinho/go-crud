94b52ff8a6543aa4120cff2e4385cc54dba14a18
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 18:42:04 -0300

    refactor: improve error handling and ID validation in DeleteUser and UpdateUser functionality

--- src/controller/user/deleteUser.go
+++ src/controller/user/deleteUser.go
@@ -4,12 +4,22 @@ import (
 	"net/http"
 
 	"github.com/devSobrinho/go-crud/src/configuration/logger"
+	"github.com/devSobrinho/go-crud/src/configuration/rest_err"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 )
 
 func (u *userController) DeleteUser(c *gin.Context) {
 	userId := c.Param("userId")
+
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		logger.Error("Erro ao tentar validar o ID do usuário", err, zap.String("journey", "deleteUser"))
+
+		errRest := rest_err.NewBadRequestError("ID do usuário inválido")
+		c.JSON(errRest.Code, errRest)
+		return
+	}
 
 	response, err := u.service.DeleteUser(userId)
 	if err != nil {
--- src/controller/user/updateUser.go
+++ src/controller/user/updateUser.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 
 	"github.com/devSobrinho/go-crud/src/configuration/logger"
+	"github.com/devSobrinho/go-crud/src/configuration/rest_err"
 	"github.com/devSobrinho/go-crud/src/configuration/validation"
 	request "github.com/devSobrinho/go-crud/src/controller/model/request/user"
 	model "github.com/devSobrinho/go-crud/src/model/user"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 )
 
@@ -23,6 +25,14 @@ func (u *userController) UpdateUser(c *gin.Context) {
 	}
 
 	userId := c.Param("userId")
+
+	if _, err := primitive.ObjectIDFromHex(userId); err != nil {
+		logger.Error("Erro ao tentar validar o ID do usuário", err, zap.String("journey", "updateUser"))
+
+		errRest := rest_err.NewBadRequestError("ID do usuário inválido")
+		c.JSON(errRest.Code, errRest)
+		return
+	}
 
 	domain := model.NewUserUpdateDomain(
 		userRequest.Name,
