908a8b1520d93b813da937f526e3e0064a3cfe52
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-25 21:05:36 -0300

    feat: implementas logs pra o restante da aplicação

--- src/database/mongodb/mongodb_connection.go
+++ src/database/mongodb/mongodb_connection.go
@@ -7,6 +7,7 @@ import (
 	"github.com/devSobrinho/go-crud/src/configuration/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
 )
 
 var (
@@ -20,14 +21,16 @@ func NewMongoDBConnection(ctx context.Context) (*mongo.Database, error) {
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
+		logger.Error("Erro ao conectar ao MongoDB", err, zap.String("journey", "NewMongoDBConnection"))
 		return nil, err
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
+		logger.Error("Erro ao fazer ping no banco de dados", err, zap.String("journey", "NewMongoDBConnection"))
 		return nil, err
 	}
 
-	logger.Info("Conectado ao MongoDB")
+	logger.Info("Conectado ao MongoDB", zap.String("journey", "NewMongoDBConnection"))
 
 	return client.Database(mongoDB), nil
 }
--- src/model/user/repository/create_user_repository.go
+++ src/model/user/repository/create_user_repository.go
@@ -7,6 +7,7 @@ import (
 	"github.com/devSobrinho/go-crud/src/configuration/logger"
 	"github.com/devSobrinho/go-crud/src/configuration/rest_err"
 	model "github.com/devSobrinho/go-crud/src/model/user"
+	"go.uber.org/zap"
 )
 
 var (
@@ -16,22 +17,27 @@ var (
 func (ur *userRepository) CreateUser(
 	userDomain model.UserDomainInterface,
 ) (model.UserDomainInterface, *rest_err.RestErr) {
-	logger.Info("Init createUser repository")
+	logger.Info("Inicia createUser repository", zap.String("journey", "createUser"))
 
 	collectionName := os.Getenv(ENV_MONGO_COLLECTION_USER)
 
 	collection := ur.databaseConnection.Collection(collectionName)
 	value, err := userDomain.GetJSONValue()
 	if err != nil {
+		logger.Error("Erro ao tentar chamar GetJSONValue", err, zap.String("journey", "createUser"))
+
 		return nil, rest_err.NewInternalServerError(err.Error())
 	}
 
 	result, err := collection.InsertOne(context.Background(), value)
 
 	if err != nil {
+		logger.Error("Erro ao tentar inserir dados a colletion de user", err, zap.String("journey", "createUser"))
 		return nil, rest_err.NewInternalServerError(err.Error())
 	}
 	userDomain.SetID(result.InsertedID.(string))
+
+	logger.Info("CreateUser repository executado com sucesso", zap.String("journey", "createUser"))
 
 	return userDomain, nil
 }
