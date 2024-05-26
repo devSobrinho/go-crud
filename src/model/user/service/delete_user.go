a3ba97eeba475fa1bede6f8dc575ad79fc252798
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 18:07:30 -0300

    refactor: improve error handling and ID validation in DeleteUser functionality

--- src/model/user/service/delete_user.go
+++ src/model/user/service/delete_user.go
@@ -9,6 +9,11 @@ import (
 func (s *userDomainService) DeleteUser(id string) (string, *rest_err.RestErr) {
 	logger.Info("DeleteUser model", zap.String("journey", "deleteUser"))
 
+	if _, err := s.userRepository.FindUserById(id); err != nil {
+		errRest := rest_err.NewNotFoundError("Usuário não encontrado")
+		return "", errRest
+	}
+
 	response, err := s.userRepository.DeleteUser(id)
 	if err != nil {
 		logger.Error("Erro ao tentar chamar DeleteUser repository", err, zap.String("journey", "deleteUser"))
