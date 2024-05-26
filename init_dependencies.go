f8010727f81d9319ed5a420147a265a624007e6b
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 11:01:39 -0300

    change: init_dependencies para o arquivo de main para funcionar na versao do go

--- init_dependencies.go
+++ init_dependencies.go
@@ -1,17 +1,19 @@
 package main
 
-import (
-	controller "github.com/devSobrinho/go-crud/src/controller/user"
-	"github.com/devSobrinho/go-crud/src/model/user/repository"
-	"github.com/devSobrinho/go-crud/src/model/user/service"
-	"go.mongodb.org/mongo-driver/mongo"
-)
+// import (
+// 	"github.com/devSobrinho/go-crud/src/configuration/logger"
+// 	controller "github.com/devSobrinho/go-crud/src/controller/user"
+// 	"github.com/devSobrinho/go-crud/src/model/user/repository"
+// 	"github.com/devSobrinho/go-crud/src/model/user/service"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.uber.org/zap"
+// )
 
-func initDependencies(database *mongo.Database) controller.UserControllerInterface {
+// func initDependencies(database *mongo.Database) controller.UserControllerInterface {
+// 	logger.Info("Iniciando dependências", zap.String("journey", "initDependencies"))
+// 	repo := repository.NewUserRepository(database)
+// 	userService := service.NewUserDomainService(repo)
+// 	userController := controller.NewUserControllerInterface(userService)
 
-	repo := repository.NewUserRepository(database)
-	userService := service.NewUserDomainService(repo)
-	userController := controller.NewUserControllerInterface(userService)
-
-	return userController
-}
+// 	return userController
+// }
--- main.go
+++ main.go
@@ -5,11 +5,26 @@ import (
 	"log"
 	"os"
 
+	"github.com/devSobrinho/go-crud/src/configuration/logger"
 	"github.com/devSobrinho/go-crud/src/controller/routes"
+	controller "github.com/devSobrinho/go-crud/src/controller/user"
 	"github.com/devSobrinho/go-crud/src/database/mongodb"
+	"github.com/devSobrinho/go-crud/src/model/user/repository"
+	"github.com/devSobrinho/go-crud/src/model/user/service"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
 )
+
+func initDependencies(database *mongo.Database) controller.UserControllerInterface {
+	logger.Info("Iniciando dependências", zap.String("journey", "initDependencies"))
+	repo := repository.NewUserRepository(database)
+	userService := service.NewUserDomainService(repo)
+	userController := controller.NewUserControllerInterface(userService)
+
+	return userController
+}
 
 func main() {
 	err := godotenv.Load()
@@ -21,6 +36,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err.Error())
 	}
+
+	// userRepository := repository.NewUserRepository(database)
+	// userService := service.NewUserDomainService(userRepository)
+	// userController := controller.NewUserControllerInterface(userService)
 
 	userController := initDependencies(database)
 
