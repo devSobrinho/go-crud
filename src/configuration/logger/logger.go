3cb9f900f726187c7cbd367b1929b02f3d375f8e
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 23:07:08 -0300

    refactor: Update logger initialization to include log activation based on environment variable

--- src/configuration/logger/logger.go
+++ src/configuration/logger/logger.go
@@ -10,10 +10,12 @@ import (
 )
 
 var (
-	log *zap.Logger
+	log    *zap.Logger
+	active bool
 )
 
-func init() {
+func InitLogger() {
+	active = strings.TrimSpace(os.Getenv(constants.ENV_LOG_ACTIVE)) == "dev"
 	logConfig := zap.Config{
 		OutputPaths: []string{getOutputLogs()},
 		Level:       zap.NewAtomicLevelAt(getLevelLogs()),
@@ -32,11 +34,17 @@ func init() {
 }
 
 func Info(message string, tags ...zap.Field) {
+	if !active {
+		return
+	}
 	log.Info(message, tags...)
 	log.Sync()
 }
 
 func Error(message string, err error, tags ...zap.Field) {
+	if !active {
+		return
+	}
 	tags = append(tags, zap.NamedError("error", err))
 	log.Info(message, tags...)
 	log.Sync()
