59cf366a94f885cde6489eccfd9794ab85593029
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 21:20:24 -0300

    refactor: Update utils.go to include token validation and claims extraction

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,7 +1,39 @@
 package utils
 
-import "strings"
+import (
+	"strings"
 
-func RemoveBearerPrefix(token string) string {
+	"github.com/devSobrinho/go-crud/src/configuration/logger"
+	"github.com/devSobrinho/go-crud/src/configuration/rest_err"
+	"github.com/golang-jwt/jwt/v5"
+	"go.uber.org/zap"
+)
+
+func removeBearerPrefix(token string) string {
 	return strings.TrimPrefix(token, "Bearer ")
+}
+
+func ValidationTokenAndExtractClaims(secretKey string, tokenString string) (jwt.MapClaims, *rest_err.RestErr) {
+	logger.Info("Inicia validação do token", zap.String("journey", "validationTokenAndExtractClaims"))
+
+	secretKeyByte := []byte(secretKey)
+	token, err := jwt.Parse(removeBearerPrefix(tokenString), func(token *jwt.Token) (interface{}, error) {
+		return secretKeyByte, nil
+	})
+
+	if err != nil {
+		logger.Error("Erro ao verificar token", err, zap.String("journey", "validationTokenAndExtractClaims"))
+		errRest := rest_err.NewUnauthorizedError("Token inválido")
+		return nil, errRest
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		logger.Error("Token inválido", err, zap.String("journey", "validationTokenAndExtractClaims"))
+		errRest := rest_err.NewUnauthorizedError("Token inválido")
+		return nil, errRest
+	}
+
+	logger.Info("Token validado com sucesso", zap.String("journey", "validationTokenAndExtractClaims"))
+	return claims, nil
 }
