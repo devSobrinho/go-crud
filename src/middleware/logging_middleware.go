abc0838113f1d2ec92515cebb370a74f1eb241e7
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-26 21:20:29 -0300

    refactor: Update logging middleware to validate and extract token claims

--- src/middleware/logging_middleware.go
+++ src/middleware/logging_middleware.go
@@ -4,36 +4,19 @@ import (
 	"os"
 
 	constants "github.com/devSobrinho/go-crud/src/configuration/contants"
-	"github.com/devSobrinho/go-crud/src/configuration/logger"
-	"github.com/devSobrinho/go-crud/src/configuration/rest_err"
 	userRequest "github.com/devSobrinho/go-crud/src/controller/model/request/user"
 	"github.com/devSobrinho/go-crud/src/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"go.uber.org/zap"
 )
 
 func Logging(c *gin.Context) {
-	authHeader := c.GetHeader("Authorization")
+	token := c.GetHeader("Authorization")
 
-	secretKey := []byte(os.Getenv(constants.ENV_JWT_SECRET))
-	token, err := jwt.Parse(utils.RemoveBearerPrefix(authHeader), func(token *jwt.Token) (interface{}, error) {
-		return secretKey, nil
-	})
+	secretKey := os.Getenv(constants.ENV_JWT_SECRET)
 
+	claims, err := utils.ValidationTokenAndExtractClaims(secretKey, token)
 	if err != nil {
-		logger.Error("Erro ao verificar token", err, zap.String("journey", "veryfyToken"))
-		errRest := rest_err.NewUnauthorizedError("Token inválido")
-		c.JSON(errRest.Code, errRest)
-		c.Abort()
-		return
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok || !token.Valid {
-		logger.Error("Token inválido", err, zap.String("journey", "veryfyToken"))
-		errRest := rest_err.NewUnauthorizedError("Token inválido")
-		c.JSON(errRest.Code, errRest)
+		c.JSON(err.Code, err)
 		c.Abort()
 		return
 	}
