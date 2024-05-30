2679a55ce7b899c7b7a4a947e03863a480f24842
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-05-30 19:40:33 -0300

    feat: Add SSE controller and dependencies for server-sent events support

--- src/configuration/dependencies/init_dependencies.go
+++ src/configuration/dependencies/init_dependencies.go
@@ -3,7 +3,9 @@ package dependencies
 import (
 	"github.com/devSobrinho/go-crud/src/configuration/logger"
 	authController "github.com/devSobrinho/go-crud/src/controller/auth"
+	sseController "github.com/devSobrinho/go-crud/src/controller/sse"
 	userController "github.com/devSobrinho/go-crud/src/controller/user"
+	serviceSSE "github.com/devSobrinho/go-crud/src/model/sse/service"
 	"github.com/devSobrinho/go-crud/src/model/user/repository"
 	"github.com/devSobrinho/go-crud/src/model/user/service"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,6 +15,7 @@ import (
 type Dependencies struct {
 	UserController userController.UserControllerInterface
 	AuthController authController.AuthControllerInterface
+	SSEController  sseController.SSEControllerInterface
 }
 
 func InitDependencies(database *mongo.Database) Dependencies {
@@ -21,8 +24,12 @@ func InitDependencies(database *mongo.Database) Dependencies {
 	userService := service.NewUserDomainService(userRepository)
 	userController := userController.NewUserControllerInterface(userService)
 	authController := authController.NewAuthControllerInterface(userService)
+
+	serviceSSE := serviceSSE.NewSSEService()
+	sseController := sseController.NewSSEControllerInterface(serviceSSE)
 	return Dependencies{
 		UserController: userController,
 		AuthController: authController,
+		SSEController:  sseController,
 	}
 }
